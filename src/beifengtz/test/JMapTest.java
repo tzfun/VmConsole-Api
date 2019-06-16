58a5cb978a90345cfbd89e724e0554cc0436e111
tzfun <1246886075@qq.com>
2019-06-16 12:53:31 +0800

    新增jmap命令，支持保存dump文件

---
+++ src/beifengtz/test/JMapTest.java
@@ -0,0 +1,16 @@
+package beifengtz.test;
+
+import beifengtz.vmconsole.JMapCmd;
+
+/**
+ * @author beifengtz
+ * <a href='http://www.beifengtz.com'>www.beifengtz.com</a>
+ * <p>location: beifengtz.test.VmConsole-Api</p>
+ * Created in 12:12 2019/6/16
+ */
+public class JMapTest {
+    public static void main(String[] args) throws Exception{
+//        JMapCmd.run(new String[]{"-dump:live,format=b,file=test.bin","8468"});
+        System.out.println(JMapCmd.dumpAll(8468, "c:/users/tzplay/desktop/test.bin"));
+    }
+}
---
+++ src/beifengtz/vmconsole/entity/jmap/JMapResult.java
@@ -0,0 +1,13 @@
+package beifengtz.vmconsole.entity.jmap;
+
+import beifengtz.vmconsole.entity.JvmResult;
+
+/**
+ * @author beifengtz
+ * <a href='http://www.beifengtz.com'>www.beifengtz.com</a>
+ * <p>location: beifengtz.vmconsole.entity.jmap.VmConsole-Api</p>
+ * Created in 11:27 2019/6/16
+ */
+public class JMapResult extends JvmResult {
+
+}
